e10c307663a145387571b142a911885c9646be48
tkohlbeck <tdkohlbeck@gmail.com>
2015-09-11 20:47:01 -0700

    public example files and helpful comments!

--- src/RandomNotes.jsx
+++ src/RandomNotes.jsx
@@ -55,8 +55,8 @@ var RandomNotes = React.createClass({
   ////// LIFECYCLE METHODS
   getInitialState: function() {
     return {
-      accidentals: false,
       accidental: '',
+      accidentals: false,
       beat: 0,
       bpm: 120,
       note: '',
@@ -88,7 +88,7 @@ var RandomNotes = React.createClass({
   },
 
   ////// SNOWFLAKE METHODS
-  tick: function() {
+  tick: function() { // called by componentDidMount, componentDidUpdate
     if (this.state.sound) { this.audio.play(); }
 
     this.setState(function(prevState) {
@@ -110,7 +110,7 @@ var RandomNotes = React.createClass({
     });
   },
 
-  bpmToMs: function() {
+  bpmToMs: function() { // called by componentDidMount, componentDidUpdate
     var milliseconds = 1 / ( this.state.bpm * (1/60000) );
         // ms / beat = 1 / (  beats / min  *  min / ms  )
     if (milliseconds <= 60000) { // if at least 1 bpm
@@ -120,7 +120,7 @@ var RandomNotes = React.createClass({
     }
   },
 
-  getRandomNote: function() { // called by componentWillMount, tick
+  getRandomNote: function() { // called by componentWillMount, tick, self
     var notes = ['A','B','C','D','E','F','G'];
     var randomIndex = Math.floor( 7*Math.random() ); // int [0,6]
     var note = notes[randomIndex];
@@ -133,7 +133,7 @@ var RandomNotes = React.createClass({
     return note;
   },
 
-  getRandomAccidental: function() {
+  getRandomAccidental: function() { // called by tick, self
     var accidentals = ['','♯','♭'];
     var randomIndex = Math.floor( 3*Math.random() ); // int [0,2]
     var accidental = accidentals[randomIndex];
@@ -146,7 +146,7 @@ var RandomNotes = React.createClass({
     return accidental;
   },
 
-  getNextBeat: function() {
+  getNextBeat: function() { // called by componentWillMount, tick
     var nextBeat = this.state.beat + 1;
     if (nextBeat === 5) {
       return 1;
@@ -154,7 +154,7 @@ var RandomNotes = React.createClass({
     return nextBeat; // 1, 2, 3, 4, 1, ...
   },
 
-  beatToDivs: function(beat) { // called by tick
+  beatToDivs: function(beat) { // called by render
     var beatBoolArray = [0,0,0,0];
     beatBoolArray[beat-1] = true;
     beatDivs = beatBoolArray.map(function(currBeat) {
@@ -168,16 +168,16 @@ var RandomNotes = React.createClass({
     return beatDivs;
   },
 
-  changeBpm: function(e) {
+  changeBpm: function(e) { // called by BpmInput.render
     this.prevBpm = this.state.bpm;
     this.setState( {bpm: e.target.value} );
   },
 
-  changeAccidentals: function(e) {
+  changeAccidentals: function(e) { // called by AccidentalsInput.render
     this.setState( {accidentals: e.target.checked} );
   },
 
-  changeSound: function(e) {
+  changeSound: function(e) { // called by SoundInput.render
     this.setState( {sound: e.target.checked} );
   },
 
